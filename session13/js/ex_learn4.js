cf9f4c2477616560fdfe687161b42bde6467b9e4
Chad Luong <apple.luong1905@gmail.com>
2019-04-06 19:36:05 +0700

    update

---
+++ session13/js/ex_learn4.js
@@ -0,0 +1,18 @@
+function checkMyName() {
+	var name = document.getElementById('name').value;
+	if (name == '') {
+		document.getElementById('errorName').innerHTML = 'Please input your name!';
+	} else {
+		document.getElementById('errorName').innerHTML = '';
+		// khi nay moi xu ly
+		var spaceFirstName = name.indexOf(' ');
+		var spaceLastName = name.lastIndexOf(' ');
+		var FirstName = name.substring(0, spaceFirstName);
+		var MidName = name.substring(spaceFirstName, spaceLastName);
+		var LastName = name.substring(spaceLastName, name.length);
+		document.getElementById('ex1').innerHTML = 'Ho la: '+ FirstName + ' <br> Ten dem la: '
+			+ MidName + '<br> Ten la: ' + LastName;
+	}
+
+
+}

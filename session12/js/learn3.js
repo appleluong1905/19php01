e17b4613f37ccb82f5cf1e0eee4a42a876bf89fb
Chad Luong <apple.luong1905@gmail.com>
2019-04-06 19:08:38 +0700

    update

--- session12/js/learn3.js
+++ session12/js/learn3.js
@@ -2,5 +2,30 @@ function sumNumber() {
 	var number1, number2;
 	number1 = document.getElementById('number1').value;
 	number2 = document.getElementById('number2').value;
-	document.getElementById('result').innerHTML = parseInt(number1) + parseInt(number2);
+	if (number1 == '') {
+		document.getElementById('result').innerHTML = 'Vui long dien so day du';
+	} else if (number2 == '') {
+		document.getElementById('result').innerHTML = 'Vui long dien so day du';
+	} else {
+		document.getElementById('result').innerHTML = parseInt(number1) + parseInt(number2);
+	}
+}
+function subNumber() {
+	var number1, number2;
+	number1 = document.getElementById('number1').value;
+	number2 = document.getElementById('number2').value;
+	var sub = parseInt(number1) - parseInt(number2);
+	document.getElementById('resultSub1').innerHTML = sub;
+}
+function multiNumber() {
+	var number1, number2;
+	number1 = document.getElementById('number1').value;
+	number2 = document.getElementById('number2').value;
+	document.getElementById('result2').innerHTML = parseInt(number1) * parseInt(number2);
+}
+function diviNumber() {
+	var number1, number2;
+	number1 = document.getElementById('number1').value;
+	number2 = document.getElementById('number2').value;
+	document.getElementById('result3').innerHTML = parseInt(number1) / parseInt(number2);
 }

6b665cf97e8dad1586422932e4061078bdb57dba
Chad Luong <apple.luong1905@gmail.com>
2019-04-12 19:40:44 +0700

    upodate

--- session14/js/learn5.js
+++ session14/js/learn5.js
@@ -29,3 +29,46 @@ function checkRegister() {
 		document.getElementById('registerSuccess').innerHTML = '';
 	}
 }
+
+///
+// BT1: Bình có 27 quyển sách, Bình có số sách gấp 3 lần số sách của Minh. 
+// Hỏi Bình phải chuyển cho 
+// Minh bao nhiêu quyển sách để số sách của Bình gấp đôi số sách của Minh?
+var BinhBook = 27;
+var MinhBook;
+MinhBook = BinhBook/3;
+var SoLanChuyen = 0;
+while (BinhBook != MinhBook*2) {
+	BinhBook--;
+	MinhBook++;
+	SoLanChuyen++;
+}
+
+document.write('<br>');
+document.write("So sach Binh chuyen cho Minh la: " + SoLanChuyen);
+
+// BT2: Cho ban 2000 vnđ đi mua kẹo .Biết :
+// 1 viên kẹo giá 200 vnđ.
+// cứ 2 vỏ kẹo đổi được 1 viên.
+// Hỏi với 2000 vnđ, ban sẽ mua đc bao nhiêu viên kẹo ?
+
+var soTien = 2000;
+var soKeo = 0;
+var soVoKeo = 0;
+while (soTien >= 200 ) {
+	soTien-=200;
+	soKeo++;
+	soVoKeo++;
+
+	while (soVoKeo >= 2) {
+		soVoKeo-=2;
+		soKeo++;
+		soVoKeo++;
+	}
+
+}
+
+document.write('<br>');
+document.write("So keo mua duoc la: " + soKeo);
+
+

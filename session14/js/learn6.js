1ebc5d9ea1395139d5cc932a7fcd2d1af4ebc5ca
Chad Luong <apple.luong1905@gmail.com>
2019-04-12 20:25:47 +0700

    upodate

--- session14/js/learn6.js
+++ session14/js/learn6.js
@@ -7,24 +7,28 @@ function changeSlider(image, id) {
 					document.getElementById('messi').style.color = "black";
 					document.getElementById('kaka').style.color = "black";
 					document.getElementById('ronaldo').style.color = "black";
+					document.getElementById('information').innerHTML = "This is De Gea, Manchester United FC";
 					break;
 			case 'messi':
 					document.getElementById('messi').style.color = "red";
 					document.getElementById('degea').style.color = "black";
 					document.getElementById('kaka').style.color = "black";
 					document.getElementById('ronaldo').style.color = "black";
+					document.getElementById('information').innerHTML = "This is Messi, Barcelona FC";
 					break;
 			case 'kaka':
 					document.getElementById('kaka').style.color = "red";
 					document.getElementById('messi').style.color = "black";
 					document.getElementById('degea').style.color = "black";
 					document.getElementById('ronaldo').style.color = "black";
+					document.getElementById('information').innerHTML = "This is Kaka, Real FC";
 					break;
 			case 'ronaldo':
 					document.getElementById('ronaldo').style.color = "red";
 					document.getElementById('messi').style.color = "black";
 					document.getElementById('kaka').style.color = "black";
 					document.getElementById('degea').style.color = "black";
+					document.getElementById('information').innerHTML = "This is Ronaldo, Juventus FC";
 					break;
 	}
 

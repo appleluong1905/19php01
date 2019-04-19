6976fc5501aebd100277d3d3dff9312bb67ec410
Chad Luong <apple.luong1905@gmail.com>
2019-04-19 18:56:02 +0700

    update

---
+++ session15/js/calculator.js
@@ -0,0 +1,15 @@
+var resultValue = ""; 
+var result = 0;
+function addValue(value) {
+
+	resultValue+=value;
+	
+	document.getElementById('result').innerHTML = resultValue;
+
+}
+
+function calValue() {
+	result = eval(resultValue);
+	document.getElementById('result').innerHTML = result;
+	resultValue = result;
+}
